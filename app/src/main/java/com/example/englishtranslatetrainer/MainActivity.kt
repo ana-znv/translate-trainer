2d76a8da93adc678f20d0881c4dc3b5fcdbbab97
Vilgefortz13 <rinatuzanzaev0@gmail.com>
2025-02-15 20:56:16 +0600

    created home screen

--- app/src/main/java/com/example/englishtranslatetrainer/MainActivity.kt
+++ app/src/main/java/com/example/englishtranslatetrainer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.englishtranslatetrainer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,10 +8,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.englishtranslatetrainer.ui.screen.HomeScreen
 import com.example.englishtranslatetrainer.ui.theme.EnglishTranslateTrainerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +23,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             EnglishTranslateTrainerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(modifier = Modifier.padding(innerPadding)) {
+                        Main()
+                    }
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EnglishTranslateTrainerTheme {
-        Greeting("Android")
+fun Main() {
+    Scaffold {
+        HomeScreen()
     }
 }
