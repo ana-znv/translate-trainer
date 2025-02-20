4e355cc765118b3bbf67b974627a39e7da2ba2c0
Vilgefortz13 <rinatuzanzaev0@gmail.com>
2025-02-20 20:04:20 +0600

    styled inputs and button

--- app/src/main/java/com/example/englishtranslatetrainer/ui/screen/AddSentenceScreen.kt
+++ app/src/main/java/com/example/englishtranslatetrainer/ui/screen/AddSentenceScreen.kt
@@ -1,13 +1,11 @@
 package com.example.englishtranslatetrainer.ui.screen
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -18,9 +16,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,9 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -73,26 +69,43 @@ fun AddSentenceScreen(navController: NavController) {
          modifier = Modifier.padding(innerPadding)
       ) {
          Spacer(Modifier.height(30.dp))
-         NativeSentence()
+         InputForSentence("Native Sentence")
+         Spacer(Modifier.height(30.dp))
+         InputForSentence("Foreign Sentence")
+         Spacer(Modifier.height(50.dp))
+         Button(
+            modifier = Modifier
+               .fillMaxWidth()
+               .padding(horizontal = 20.dp)
+               .height(60.dp),
+            onClick = {}
+         ) {
+            Text(
+               "Done",
+               style = TextStyle(
+                  fontSize = 30.sp,
+                  fontWeight = FontWeight.Medium
+               )
+            )
+         }
       }
    }
 }
 
 @Composable
-fun NativeSentence() {
+fun InputForSentence(contentLabel: String) {
    var text by remember { mutableStateOf("") }
-   var result by remember { mutableStateOf("") }
 
    Column {
       OutlinedTextField(
          modifier = Modifier
-           .fillMaxWidth()
-           .padding(horizontal = 20.dp),
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
          value = text,
          onValueChange = { text = it },
          label = {
             Text(
-               "Native Sentence",
+               contentLabel,
                style = TextStyle(
                   fontSize = 20.sp
                )
@@ -101,15 +114,5 @@ fun NativeSentence() {
          maxLines = Int.MAX_VALUE,
          keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Default)
       )
-      Spacer(Modifier.height(10.dp))
-      Button(
-         modifier = Modifier
-           .fillMaxWidth()
-           .padding(horizontal = 20.dp),
-         onClick = { result = text }
-      ) {
-         Text("click")
-      }
-      Text(text = result)
    }
 }
