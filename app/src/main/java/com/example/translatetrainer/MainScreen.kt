613a2cd25e21d83bc8b0e3f58eb373d1d6540b07
Vilgefortz13 <rinatuzanzaev0@gmail.com>
2025-03-03 19:50:13 +0600

    created main screen

--- app/src/main/java/com/example/translatetrainer/MainScreen.kt
+++ app/src/main/java/com/example/translatetrainer/MainScreen.kt
@@ -1,17 +1,23 @@
 package com.example.translatetrainer
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainScreen() {
@@ -23,12 +29,22 @@ fun MainScreen() {
 
 @Composable
 fun MainElements(text: String) {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(8.dp)
-        .background(MaterialTheme.colorScheme.primary)
-        .clip(RoundedCornerShape(16.dp))
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.primary)
+            .height(80.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = text)
+        Text(
+            text = text,
+            style = TextStyle(
+                fontSize = 30.sp,
+                color = Color.White
+            )
+        )
     }
 }
